fad9c16368132ce38abf394e6143907383916785
bv9306 <88138792+bv9306@users.noreply.github.com>
2021-12-08 23:56:40 +0300

    Docker File prepared

--- burakvuralvoicespin/init_task_voicespin.sh
+++ burakvuralvoicespin/init_task_voicespin.sh
@@ -4,6 +4,6 @@ echo "Voicespin settings file is $DJANGO_SETTINGS_MODULE"
 set -x
 python manage.py makemigrations
 python manage.py migrate
-python /burakvuralvoicespin-test/helperservices/background_processor.py &
+python /burakvuralvoicespin-test/helper_services/background_processor.py &
 python manage.py process_tasks >/dev/null 2>&1 &
 python manage.py runserver 0.0.0.0:7000
