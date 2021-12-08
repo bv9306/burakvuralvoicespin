b7093e97ff03528dd1d70aaed2077702d4ae7f32
bv9306 <88138792+bv9306@users.noreply.github.com>
2021-12-08 19:18:32 +0300

    allowed hosts have been set as *

--- burakvuralvoicespin/settings.py
+++ burakvuralvoicespin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@l)rwi-6pegwln9qea6f^pvxhvaews2pb19%eyt7z%sgo(3k1m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
