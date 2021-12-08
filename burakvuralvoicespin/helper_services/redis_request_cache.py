c15620fd67b1830e456e374d6b33bdc6eba76d36
bv9306 <88138792+bv9306@users.noreply.github.com>
2021-12-08 19:21:44 +0300

    all of the images will take its values from os environment

--- burakvuralvoicespin/helper_services/redis_request_cache.py
+++ burakvuralvoicespin/helper_services/redis_request_cache.py
@@ -1,10 +1,11 @@
 import json
+import os
 import uuid
 
 import redis
 
-redis_host = "192.168.48.1"
-redis_port = 6379
+redis_host = os.environ['REDIS_HOST']
+redis_port = os.environ['REDIS_PORT']
 redis_password = ""
 red_client = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
 
--- burakvuralvoicespin/settings.py
+++ burakvuralvoicespin/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,9 +79,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'task-voicespin',
-        'USER': 'postgres',
-        'PASSWORD': '31415',
-        'HOST': 'localhost',
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
         'PORT': '5432',
         'TEST': {
             'NAME': 'task-voicespin',
