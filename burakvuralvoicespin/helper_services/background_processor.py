35a91dba4d81051219b55fc0aedd728ad9b909c7
bv9306 <88138792+bv9306@users.noreply.github.com>
2021-12-09 10:23:46 +0300

    multiprocessor has been added

--- burakvuralvoicespin/helper_services/background_processor.py
+++ burakvuralvoicespin/helper_services/background_processor.py
@@ -2,6 +2,10 @@ from background_task import background
 
 from async_request_sender import make_heavy_operation
 from redis_request_cache import red_client
+from multiprocessing import Process
+import multiprocessing
+
+processes = []
 
 
 def evaluate_heavy_operations_request_to_database():
@@ -12,8 +16,19 @@ def evaluate_heavy_operations_request_to_database():
                 type = red_client.type(key)
                 if type == "list":
                     value = red_client.lrange(key, 0, -1)
-                    make_heavy_operation(value)
-                    red_client.delete(key)
+                    for i in range(multiprocessing.cpu_count()):
+                        processes.append(Process(target=make_heavy_operation(value)))
+                        make_heavy_operation(value)
+                        red_client.delete(key)
+
+
+def start_processes_as_multithread(processes):
+    if processes is not None and len(processes) > 0:
+        for process in processes:
+            process.start()
+        for process in processes:
+            process.join()
 
 
 evaluate_heavy_operations_request_to_database()
+start_processes_as_multithread(processes)
