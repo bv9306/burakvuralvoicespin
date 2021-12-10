e05783021f63d2e9636eabca81a3030661a09bc7
bv9306 <88138792+bv9306@users.noreply.github.com>
2021-12-10 17:43:31 +0300

    process have been updated

--- burakvuralvoicespin/helper_services/background_processor.py
+++ burakvuralvoicespin/helper_services/background_processor.py
@@ -8,23 +8,26 @@ from redis_request_cache import red_client
 
 def evaluate_heavy_operations_request_to_database():
     while True:
-        time.sleep(5)
-        keys = red_client.keys()
-        if keys is not None:
-            for key in keys:
-                type = red_client.type(key)
-                if type == "list":
-                    value = red_client.lrange(key, 0, -1)
-                    make_heavy_operation(value)
-                    red_client.delete(key)
+        try:
+            time.sleep(5)
+            keys = red_client.keys()
+            if keys is not None:
+                for key in keys:
+                    type = red_client.type(key)
+                    if type == "list":
+                        value = red_client.lrange(key, 0, -1)
+                        make_heavy_operation(value)
+                        red_client.delete(key)
 
-        if processes is not None and len(processes) > 0:
-            for process in processes:
-                process.start()
-            for process in processes:
-                process.join()
-                print('registering next process')
-                processes.remove(process)
+            if processes is not None and len(processes) > 0:
+                for process in processes:
+                    process.start()
+                for process in processes:
+                    process.join()
+                    print('registering next process')
+                    processes.remove(process)
+        except Exception as e:
+            print(e)
 
 
 evaluate_heavy_operations_request_to_database()
